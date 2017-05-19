4d96333d2d02f57d2d7ab184996a6411e5efbf42
caseylholston <caseylholston@gmail.com>
2017-05-19 19:49:52 -0400

    Added auth/facebook/callback

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
     "facebookAuth": {
         "clientID": "782300481929581",
         "clientSecret": "76d106546e5c13c21831f950167af30a",
-        "callbackURL": "https://shielded-citadel-36222.herokuapp.com"
+        "callbackURL": "https://shielded-citadel-36222.herokuapp.com/auth/facebook/callback"
     },
     "googleAuth": {
         "clientID": "151060501257-nj0s9ae4itk3gtm4p9f0nu3mt80437br.apps.googleusercontent.com",
