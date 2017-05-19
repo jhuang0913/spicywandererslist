f5014bd55dccb14a3bda9e2966aa91c1e2f66d5f
caseylholston <caseylholston@gmail.com>
2017-05-19 19:35:21 -0400

    Trying https://shielded-citadel-36222.herokuapp.com/auth/google/callback in the auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,13 @@ module.exports = {
     "facebookAuth": {
         "clientID": "782300481929581",
         "clientSecret": "76d106546e5c13c21831f950167af30a",
-        "callbackURL": "http://localhost:8080/auth/facebook/callback"
+        "callbackURL": "https://shielded-citadel-36222.herokuapp.com/auth/facebook/callback"
     },
     "googleAuth": {
         "clientID": "151060501257-nj0s9ae4itk3gtm4p9f0nu3mt80437br.apps.googleusercontent.com",
         "clientSecret": "1PUMcVkesURu326pgPLOGswO",
-        "callbackURL": "http://localhost:8080/auth/google/callback"
+        //"callbackURL": "http://localhost:8080/auth/google/callback"
+        "callbackURL": "https://shielded-citadel-36222.herokuapp.com/auth/google/callback"
 
     }
 }
